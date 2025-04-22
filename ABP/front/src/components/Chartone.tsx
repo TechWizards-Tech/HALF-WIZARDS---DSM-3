29ad74ac9c9ca5a329437a3647a5ffdf671881c0
gmlebc <gmlebc@gmail.com>
2025-04-22 19:59:49 -0300

    dash com 2 graf de exemplo, component chart grafico

---
+++ ABP/front/src/components/Chartone.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, LabelList, Line, LineChart, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "January", year: 2021, desktop: 186, mobile: 80 },
+{ month: "February", year: 2021, desktop: 305, mobile: 200 },
+{ month: "March", year: 2021, desktop: 237, mobile: 120 },
+{ month: "April", year: 2021, desktop: 73, mobile: 190 },
+{ month: "May", year: 2021, desktop: 209, mobile: 130 },
+{ month: "June", year: 2021, desktop: 214, mobile: 140 },
+{ month: "July", year: 2021, desktop: 198, mobile: 75 },
+{ month: "August", year: 2021, desktop: 250, mobile: 100 },
+{ month: "September", year: 2021, desktop: 276, mobile: 110 },
+{ month: "October", year: 2021, desktop: 230, mobile: 90 },
+{ month: "November", year: 2021, desktop: 210, mobile: 85 },
+{ month: "December", year: 2021, desktop: 260, mobile: 95 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig
+
+export function Chartone() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Line Chart - Label</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+
+
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 20,
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Line
+              dataKey="desktop"
+              type="natural"
+              stroke="gray"
+              strokeWidth={2}
+              dot={{
+                fill: "var(--color-desktop)",
+              }}
+              activeDot={{
+                r: 6,
+              }}
+            >
+              <LabelList
+                position="top"
+                offset={12}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Line>
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+     
+    </Card>
+  )
+}
--- ABP/front/src/components/Dashboard.tsx
+++ ABP/front/src/components/Dashboard.tsx
@@ -1,9 +1,65 @@
-import React, { Component } from 'react'
+import { Appsidebar } from "./Appsidebar"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { Chartone } from "./Chartone"
+import { Charttwo } from "./Charttwo"
+import { CloudRainWind } from "lucide-react"
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <div>Dashboard</div>
-    )
-  }
+export default function Page() {
+  return (
+
+    <SidebarProvider>
+      <Appsidebar />
+      <SidebarInset>
+
+
+        <header className="flex flex-row h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+
+          <div className="flex">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+
+          <div className="flex justify-center gap-2 md:justify-start items-end flex-row-reverse">
+            <a href="#" className="flex items-center text-2xl font-bold">
+              TechWinds
+              <CloudRainWind className="stroke-2 size-15" />
+            </a>
+          </div>
+
+        </header>
+
+        <div className="flex flex-1 flex-col gap-4 p-4">
+
+
+          <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+
+            <div className="aspect-video rounded-xl bg-muted/50 border-2  shadow-xl" />
+            <div className="aspect-video rounded-xl bg-muted/50 border-2  shadow-xl" />
+            <div className="aspect-video rounded-xl bg-muted/50 border-2  shadow-xl" />
+            <div className="aspect-video rounded-xl bg-muted/50 border-2  shadow-xl" />
+
+          </div>
+
+
+          <div className="flex gap-4">
+            <div className=" flex-1 rounded-xl bg-muted/50 md:min-h-min border-2 shadow-xl">
+              <Chartone />
+            </div>
+            <div className=" flex-1 rounded-xl bg-muted/50 md:min-h-min border-2 shadow-xl">
+              <Charttwo />
+              {/* <Chartone /> */}
+            </div>
+          </div>
+
+
+        </div>
+
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
