d44dec214e217309a65bd67cc86c5c9d28970300
gmlebc <gmlebc@gmail.com>
2025-05-15 22:13:13 -0300

    Alerta de navegação

--- ABP/front/src/components/Dashboard.tsx
+++ ABP/front/src/components/Dashboard.tsx
@@ -9,13 +9,16 @@ import { useState } from "react";
 import StationSelector from "./Stationselector";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import NavigationAlert from "./Navigationalert";
 
 export default function Dashboard() {
   const [selectedStation, setSelectedStation] = useState("estacao1");
+  const currentWaveHeight = 1.0;
+  const currentWindSpeed = 10;
 
   function generateLineChartData1() {
     return [
-      { name: "Jan", value: 186 },
+      { name: "JAN", value: 80 },
       { name: "Fev", value: 305 },
       { name: "Mar", value: 237 },
       { name: "Abr", value: 73 },
@@ -112,69 +115,81 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="sticky top-0 bg-white">
-        <header className="flex flex-row h-16 shrink-0 items-center gap-2 border-b px-4 justify-between ">
-          <div className=" not-only:flex justify-center gap-2 md:justify-start items-end flex-row-reverse">
-            <a href="#" className="flex items-center text-3xl font-bold">
-              TechWinds
-              <CloudRainWind className="stroke-2 size-15" />
-            </a>
-          </div>
-
-          <div className="flex items-center gap-10">
-            <StationSelector
-              selectedStation={selectedStation}
-              onStationChange={setSelectedStation}
-              className="hidden sm:flex"
-            />
-
-            <Button asChild className="w-64 gap-2">
-              <Link to="/login">
-                Deseja logar?
-                <LogIn className="w-4 h-4" />
-              </Link>
-            </Button>
-
-          </div>
-        </header>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 ">
-        <Wavecard currentHeight={1.5} maxHeight={3} />
-        <Windspeedcard currentSpeed={30} maxSpeed={60} />
-      </div>
-
-      <div className="custom-card p-1 flex">
-        <Customcard
-          frontContent={
-            <div className="flex flex-row w-full gap-6 place-content-around">
-              <div className="w-1/2">
-                <Linechart
-                  title="Tendência Mensal"
-                  subtitle="Janeiro - Dezembro 2024"
-                  data={lineChartData}
-                  className="animate-fade-in"
-                />
-              </div>
-              <div className="w-1/2">
-                <Areachart
-                  title="Gráfico Interativo"
-                  subtitle="Mostrando total de visitantes por período"
-                  data={areaChartData}
-                  timeRanges={timeRanges}
-                  onTimeRangeChange={handleTimeRangeChange}
-                  className="animate-fade-in"
-                />
-              </div>
+      <div>
+        <div className="sticky top-0 bg-white">
+          <header className="flex flex-row h-16 shrink-0 items-center gap-2 border-b px-4 justify-between ">
+            <div className=" not-only:flex justify-center gap-2 md:justify-start items-end flex-row-reverse">
+              <a href="#" className="flex items-center text-3xl font-bold">
+                TechWinds
+                <CloudRainWind className="stroke-2 size-15" />
+              </a>
             </div>
-          }
-          backContent={
-            <div className="mb-6">
-              <WeatherTable data={weatherData} className="animate-fade-in" />
+
+            <div className="flex items-center">
+              
             </div>
-          }
-          className="w-full"
-        />
+
+            <div className="flex items-center gap-10">
+
+            <NavigationAlert
+                waveHeight={currentWaveHeight}
+                windSpeed={currentWindSpeed}
+                className="animate-fade-in"
+              />
+
+              <StationSelector
+                selectedStation={selectedStation}
+                onStationChange={setSelectedStation}
+                className="hidden sm:flex"
+              />
+
+              <Button asChild className="w-32 gap-2">
+                <Link to="/login">
+                  Deseja logar?
+                  <LogIn className="w-4 h-4" />
+                </Link>
+              </Button>
+            </div>
+          </header>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+          <Wavecard currentHeight={1.9} maxHeight={3} />
+          <Windspeedcard currentSpeed={36} maxSpeed={60} />
+        </div>
+
+        <div className="custom-card p-1 flex">
+          <Customcard
+            frontContent={
+              <div className="flex flex-row w-full gap-6 place-content-around">
+                <div className="w-1/2">
+                  <Linechart
+                    title="Tendência Mensal"
+                    subtitle="Janeiro - Dezembro 2024"
+                    data={lineChartData}
+                    className="animate-fade-in"
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Areachart
+                    title="Gráfico Interativo"
+                    subtitle="Mostrando total de visitantes por período"
+                    data={areaChartData}
+                    timeRanges={timeRanges}
+                    onTimeRangeChange={handleTimeRangeChange}
+                    className="animate-fade-in"
+                  />
+                </div>
+              </div>
+            }
+            backContent={
+              <div className="mb-6">
+                <WeatherTable data={weatherData} className="animate-fade-in" />
+              </div>
+            }
+            className="w-full"
+          />
+        </div>
       </div>
     </>
   );
